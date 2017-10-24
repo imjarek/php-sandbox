6f467f13613a6e055d93135b0100831dbeb8279b
Sergey Cherednichenko <sergey.cherednichenko@bonusway.com>
2017-10-24 16:18:16 +0300

    9000 port is used now. OK?

---
+++ containers/fpm/folder/with_some_file.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of with_some_file
+ *
+ * @author exys
+ */
+class with_some_file {
+    //put your code here
+}
