808929224d5602fc7f3e1d0f4db4f04b454beef0
Imjarek <sergey.i.che@gmail.com>
2018-01-29 16:22:47 +0300

    master/detail component

--- code/front/src/app/app.module.ts
+++ code/front/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { UserDetailComponent } from './user-detail/user-detail.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
